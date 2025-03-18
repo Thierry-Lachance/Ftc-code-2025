5f93a351aa9bc22ab5c896da3e6d2f17b358c1b6
Thierry Lachance <lachancethierry6@gmail.com>
2025-03-18 11:44:23 -0400

    base code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto.java
@@ -0,0 +1,139 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+
+import java.util.Locale;
+
+@Autonomous(name="Pinpoint Navigation Example", group="Pinpoint")
+//@Disabled
+
+public class auto extends LinearOpMode {
+
+    DcMotor leftFrontDrive;
+    DcMotor rightFrontDrive;
+    DcMotor leftBackDrive;
+    DcMotor rightBackDrive;
+
+    GoBildaPinpointDriver odo; // Declare OpMode member for the Odometry Computer
+    DriveToPoint nav = new DriveToPoint(this); //OpMode member for the point-to-point navigation class
+
+    enum StateMachine {
+        WAITING_FOR_START,
+        AT_TARGET,
+        WAITING_FOR_TARGET,
+        DRIVE_TO_TARGET_1,
+        DRIVE_TO_TARGET_2,
+        DRIVE_TO_TARGET_3,
+        DRIVE_TO_TARGET_4
+
+    }
+
+    static final Pose2D TARGET_1 = new Pose2D(DistanceUnit.CM,0,60,AngleUnit.DEGREES,0);
+    static final Pose2D TARGET_2 = new Pose2D(DistanceUnit.CM, 60, 60, AngleUnit.DEGREES, 0);
+    static final Pose2D TARGET_3 = new Pose2D(DistanceUnit.CM,60,0, AngleUnit.DEGREES,0);
+    static final Pose2D TARGET_4 = new Pose2D(DistanceUnit.CM, 0, 0, AngleUnit.DEGREES, 0);
+
+
+
+    @Override
+    public void runOpMode() {
+
+        // Initialize the hardware variables. Note that the strings used here must correspond
+        // to the names assigned during the robot configuration step on the DS or RC devices.
+
+        leftFrontDrive  = hardwareMap.get(DcMotor.class, "flMotor");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "frMotor");
+        leftBackDrive   = hardwareMap.get(DcMotor.class, "blMotor");
+        rightBackDrive  = hardwareMap.get(DcMotor.class, "brMotor");
+
+        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        rightFrontDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightBackDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        odo = hardwareMap.get(GoBildaPinpointDriver.class,"odo");
+        odo.setOffsets(-142.0, 120.0); //these are tuned for 3110-0002-0001 Product Insight #1
+        odo.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
+        odo.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
+
+        odo.resetPosAndIMU();
+
+        //nav.setXYCoefficients(0.02,0.002,0.0,DistanceUnit.MM,12);
+        //nav.setYawCoefficients(1,0,0.0, AngleUnit.DEGREES,2);
+        nav.setDriveType(DriveToPoint.DriveType.MECANUM);
+
+        StateMachine stateMachine;
+        stateMachine = StateMachine.WAITING_FOR_START;
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.addData("X offset", odo.getXOffset());
+        telemetry.addData("Y offset", odo.getYOffset());
+        telemetry.addData("Device Version Number:", odo.getDeviceVersion());
+        telemetry.addData("Device Scalar", odo.getYawScalar());
+        telemetry.update();
+
+        // Wait for the game to start (driver presses START)
+        waitForStart();
+        resetRuntime();
+
+        while (opModeIsActive()) {
+            odo.update();
+
+            switch (stateMachine){
+                case WAITING_FOR_START:
+                    stateMachine = StateMachine.DRIVE_TO_TARGET_1;
+                    break;
+                case DRIVE_TO_TARGET_1:
+                    if (nav.driveTo(odo.getPosition(), TARGET_1, 0.5, 0.5)){
+                        telemetry.addLine("at position #1!");
+                        stateMachine = StateMachine.DRIVE_TO_TARGET_2;
+                    }
+                    break;
+                case DRIVE_TO_TARGET_2:
+                    if (nav.driveTo(odo.getPosition(), TARGET_2, 0.5, 0.5)){
+                        telemetry.addLine("at position #2!");
+                        stateMachine = StateMachine.DRIVE_TO_TARGET_3;
+                    }
+                    break;
+                case DRIVE_TO_TARGET_3:
+                    if(nav.driveTo(odo.getPosition(), TARGET_3, 0.5, 0.5)){
+                        telemetry.addLine("at position #3");
+                        stateMachine = StateMachine.DRIVE_TO_TARGET_4;
+                    }
+                    break;
+                case DRIVE_TO_TARGET_4:
+                    if(nav.driveTo(odo.getPosition(),TARGET_4,0.5,0.5)){
+                        telemetry.addLine("at position #4");
+                        stateMachine = StateMachine.AT_TARGET;
+                    }
+                    break;
+
+            }
+
+
+            //nav calculates the power to set to each motor in a mecanum or tank drive. Use nav.getMotorPower to find that value.
+            leftFrontDrive.setPower(nav.getMotorPower(DriveToPoint.DriveMotor.LEFT_FRONT));
+            rightFrontDrive.setPower(nav.getMotorPower(DriveToPoint.DriveMotor.RIGHT_FRONT));
+            leftBackDrive.setPower(nav.getMotorPower(DriveToPoint.DriveMotor.LEFT_BACK));
+            rightBackDrive.setPower(nav.getMotorPower(DriveToPoint.DriveMotor.RIGHT_BACK));
+
+            telemetry.addData("current state:",stateMachine);
+
+            Pose2D pos = odo.getPosition();
+            String data = String.format(Locale.US, "{X: %.3f, Y: %.3f, H: %.3f}", pos.getX(DistanceUnit.MM), pos.getY(DistanceUnit.MM), pos.getHeading(AngleUnit.DEGREES));
+            telemetry.addData("Position", data);
+
+            telemetry.update();
+
+        }
+    }}
