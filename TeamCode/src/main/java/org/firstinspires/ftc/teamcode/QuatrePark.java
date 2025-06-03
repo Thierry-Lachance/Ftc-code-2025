5bd9e4af2d0ad39fa020e9b12a6928130106b484
Thierry Lachance <lachancethierry6@gmail.com>
2025-06-03 19:06:08 -0400

    auto fonctionne

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/QuatrePark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/QuatrePark.java
@@ -47,22 +47,26 @@ public class QuatrePark extends LinearOpMode {
         DRIVE_TO_TARGET_6,
         DRIVE_TO_TARGET_7,
         Drive_TO_TARGET_7_1,
-        DRIVE_TO_TARGET_8
+        DRIVE_TO_TARGET_8,
+        DRIVE_TO_TARGET_9,
+        DRIVE_TO_TARGET_10
     }
 
     static final Pose2D TARGET_1 = new Pose2D(DistanceUnit.MM, 200, 0, AngleUnit.DEGREES, -20);
     static final Pose2D TARGET_2 = new Pose2D(DistanceUnit.INCH, 3.6, 35.0, AngleUnit.DEGREES, -45);
-    static final Pose2D TARGET_3 = new Pose2D(DistanceUnit.MM, 220, 620, AngleUnit.DEGREES, 0);
+    static final Pose2D TARGET_3 = new Pose2D(DistanceUnit.MM, 220, 630, AngleUnit.DEGREES, 0);
     static final Pose2D TARGET_4 = new Pose2D(DistanceUnit.INCH, 3.6, 35.0, AngleUnit.DEGREES, -45);
     static final Pose2D TARGET_5 = new Pose2D(DistanceUnit.MM, 220, 880, AngleUnit.DEGREES, 0);
     static final Pose2D TARGET_6 = new Pose2D(DistanceUnit.INCH, 3.6, 35.0, AngleUnit.DEGREES, -45);
     static final Pose2D TARGET_7 = new Pose2D(DistanceUnit.MM, 890, 340, AngleUnit.DEGREES, 90);
-    static final Pose2D TARGET_8 = new Pose2D(DistanceUnit.INCH, 3.6, 35.0, AngleUnit.DEGREES, -45);
-    static final Pose2D TARGET_3_1 = new Pose2D(DistanceUnit.MM, 380, 620, AngleUnit.DEGREES, 0);
+    static final Pose2D TARGET_8 = new Pose2D(DistanceUnit.INCH, 4.6, 36.0, AngleUnit.DEGREES, -45);
+    static final Pose2D TARGET_9 = new Pose2D(DistanceUnit.MM, 1500, 500, AngleUnit.DEGREES, -90);
+    static final Pose2D TARGET_10 = new Pose2D(DistanceUnit.MM, 1500, -25, AngleUnit.DEGREES, -90);
+    static final Pose2D TARGET_3_1 = new Pose2D(DistanceUnit.MM, 380, 630, AngleUnit.DEGREES, 0);
     static final Pose2D TARGET_5_1 = new Pose2D(DistanceUnit.MM, 380, 880, AngleUnit.DEGREES, 0);
-    static final Pose2D TARGET_7_1 = new Pose2D(DistanceUnit.MM, 890, 400, AngleUnit.DEGREES, 90);
+    static final Pose2D TARGET_7_1 = new Pose2D(DistanceUnit.MM, 890, 550, AngleUnit.DEGREES, 90);
 
-
+    double speed = 0.7;
     @Override
     public void runOpMode() {
         leftFrontDrive = hardwareMap.dcMotor.get("fl");
@@ -111,16 +115,18 @@ public class QuatrePark extends LinearOpMode {
         resetRuntime();
         servoPinceL.setPower(0);
         servoPinceR.setPower(0);
+        servoBucket.setPower(0);
         while (opModeIsActive()) {
             odo.update();
 
             switch (stateMachine) {
                 case WAITING_FOR_START://up the slider
-                    stateMachine = StateMachine.DRIVE_TO_TARGET_1;
+                    stateMachine = StateMachine.DRIVE_TO_TARGET_2;
+                    openArm();
                     upElevator();
                     break;
                 case DRIVE_TO_TARGET_1:
-                    if (nav.driveTo(odo.getPosition(), TARGET_1, 0.5, 0.0)) {
+                    if (nav.driveTo(odo.getPosition(), TARGET_1, 1.0, 0.0)) {
                         telemetry.addLine("at position #1!");
                         stateMachine = StateMachine.DRIVE_TO_TARGET_2;
 
@@ -129,13 +135,10 @@ public class QuatrePark extends LinearOpMode {
 
                     break;
                 case DRIVE_TO_TARGET_2:
-                    if (nav.driveTo(odo.getPosition(), TARGET_2, 0.5, 0.0)) {//dump in the basket and open arm
+                    if (nav.driveTo(odo.getPosition(), TARGET_2, speed, 0.0)) {//dump in the basket and open arm
                         telemetry.addLine("at position #2!");
-                        openArm();
-                        while (notReadyToDump()) {
-                            telemetry.addLine("waiting for elevator to be ready to dump");
-                            telemetry.update();
-                        }
+                        stopWheel();
+
                         flipBucket(); // dump the block in the basket
                         downElevator();
                         stopArm();
@@ -145,35 +148,31 @@ public class QuatrePark extends LinearOpMode {
 
                     break;
                 case DRIVE_TO_TARGET_3:
-                    if (nav.driveTo(odo.getPosition(), TARGET_3, 0.5, 0.0)) {//stop the arm
+                    if (nav.driveTo(odo.getPosition(), TARGET_3, speed, 0.0)) {//stop the arm
                         telemetry.addLine("at position #3");
                         stateMachine = StateMachine.Drive_TO_TARGET_3_1;
                     }
 
                     break;
                 case Drive_TO_TARGET_3_1:
-                    if (nav.driveTo(odo.getPosition(), TARGET_3_1, 0.5, 0.0)) { //Close the claw at block position
+                    if (nav.driveTo(odo.getPosition(), TARGET_3_1, speed-0.2, 0.0)) { //Close the claw at block position
                         telemetry.addLine("at position #3.1");
+                        stopWheel();
                         closeClaw();
-                        while (notReadyToPick()) {
-                            telemetry.addLine("waiting for elevator to be ready to pick");
-                            telemetry.update();
-                        }
+
                         closeArm();
                         openClaw();
                         upElevator();
+                        openArm();
                         stateMachine = StateMachine.DRIVE_TO_TARGET_4;
                     }
 
                     break;
                 case DRIVE_TO_TARGET_4:
-                    if (nav.driveTo(odo.getPosition(), TARGET_4, 0.5, 0.0)) {//dump the block in the basket
+                    if (nav.driveTo(odo.getPosition(), TARGET_4, speed, 0.0)) {//dump the block in the basket
                         telemetry.addLine("at position #4");
-                        openArm();
-                        while (notReadyToDump()) {
-                            telemetry.addLine("waiting for elevator to be ready to dump");
-                            telemetry.update();
-                        }
+                        stopWheel();
+
                         flipBucket(); // dump the block in the basket
                         downElevator();
                         stopArm();
@@ -182,34 +181,29 @@ public class QuatrePark extends LinearOpMode {
 
                     break;
                 case DRIVE_TO_TARGET_5:
-                    if (nav.driveTo(odo.getPosition(), TARGET_5, 0.5, 0.0)) {//stop the arm
+                    if (nav.driveTo(odo.getPosition(), TARGET_5, speed, 0.0)) {//stop the arm
                         telemetry.addLine("at position #5");
-
                         stateMachine = StateMachine.DRIVE_TO_TARGET_5_1;
                     }
+                    break;
                 case DRIVE_TO_TARGET_5_1:
-                    if (nav.driveTo(odo.getPosition(), TARGET_5_1, 0.5, 0.0)) {//close the claw at block position
+                    if (nav.driveTo(odo.getPosition(), TARGET_5_1, speed-0.2, 0.0)) {//close the claw at block position
                         telemetry.addLine("at position #5.1");
+                        stopWheel();
                         closeClaw();
-                        while (notReadyToPick()) {
-                            telemetry.addLine("waiting for elevator to be ready to pick");
-                            telemetry.update();
-                        }
+
                         closeArm();
                         openClaw();
                         upElevator();
+                        openArm();
                         stateMachine = StateMachine.DRIVE_TO_TARGET_6;
                     }
 
                     break;
                 case DRIVE_TO_TARGET_6:
-                    if (nav.driveTo(odo.getPosition(), TARGET_6, 0.5, 0.0)) {// dump the block in the basket
+                    if (nav.driveTo(odo.getPosition(), TARGET_6, speed, 0.0)) {// dump the block in the basket
                         telemetry.addLine("at position #6");
-                        openArm();
-                        while (notReadyToDump()) {
-                            telemetry.addLine("waiting for elevator to be ready to dump");
-                            telemetry.update();
-                        }
+                        stopWheel();
                         flipBucket(); // dump the block in the basket
                         downElevator();
                         stopArm();
@@ -218,7 +212,7 @@ public class QuatrePark extends LinearOpMode {
 
                     break;
                 case DRIVE_TO_TARGET_7:
-                    if (nav.driveTo(odo.getPosition(), TARGET_7, 0.5, 0.0)) {// stop the arm
+                    if (nav.driveTo(odo.getPosition(), TARGET_7, speed, 0.0)) {// stop the arm
                         telemetry.addLine("at position #7");
 
                         stateMachine = StateMachine.Drive_TO_TARGET_7_1;
@@ -226,32 +220,47 @@ public class QuatrePark extends LinearOpMode {
 
                     break;
                 case Drive_TO_TARGET_7_1:
-                    if (nav.driveTo(odo.getPosition(), TARGET_7_1, 0.5, 0.0)) {// close the claw at block position
+                    if (nav.driveTo(odo.getPosition(), TARGET_7_1, speed-0.2, 0.0)) {// close the claw at block position
                         telemetry.addLine("at position #7.1");
+                        stopWheel();
                         closeClaw();
-                        while (notReadyToPick()) {
-                            telemetry.addLine("waiting for elevator to be ready to pick");
-                            telemetry.update();
-                        }
+
                         closeArm();
                         openClaw();
                         upElevator();
+
                         stateMachine = StateMachine.DRIVE_TO_TARGET_8;
                     }
                     break;
                 case DRIVE_TO_TARGET_8:
-                    if (nav.driveTo(odo.getPosition(), TARGET_8, 0.5, 0.0)) {// dump the block in the basket
+                    if (nav.driveTo(odo.getPosition(), TARGET_8, speed, 0.0)) {// dump the block in the basket
                         telemetry.addLine("at position #8");
-                        while (notReadyToDump()) {
-                            telemetry.addLine("waiting for elevator to be ready to dump");
-                            telemetry.update();
-                        }
+                        stopWheel();
+
                         flipBucket(); // dump the block in the basket
                         downElevator();
-                        stateMachine = StateMachine.AT_TARGET;
+                        stateMachine = StateMachine.DRIVE_TO_TARGET_9;
 
                     }
 
+                    break;
+                case DRIVE_TO_TARGET_9:
+                    if (nav.driveTo(odo.getPosition(), TARGET_9, 1.0, 0.0)) {// stop the arm
+                        telemetry.addLine("at position #9");
+                       openClawNoStop();
+
+                        stateMachine = StateMachine.DRIVE_TO_TARGET_10;
+                    }
+                    break;
+                case DRIVE_TO_TARGET_10:
+                    if (nav.driveTo(odo.getPosition(), TARGET_10, speed+0.2, 0.0)) {// stop the arm
+                        telemetry.addLine("at position #10");
+
+                        stopWheel();
+                        openArmPowerfull();
+                        sleep(1000);
+                        stateMachine = StateMachine.AT_TARGET;
+                    }
                     break;
             }
 
@@ -278,23 +287,25 @@ public class QuatrePark extends LinearOpMode {
     }
 
     public void upElevator() {
-        slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         slideMotor.setTargetPosition(POSITION_2);
+        slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
         slideMotor.setPower(1.0);
 
     }
 
     public void downElevator() {
-        slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         slideMotor.setTargetPosition(POSITION_1);
+        slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
         slideMotor.setPower(1.0);
     }
 
     public void flipBucket() {
         servoBucket.setPower(1);
-        sleep(2000);
+        sleep(1000);
         servoBucket.setPower(-1);
-        sleep(2000);
+        sleep(800);
         servoBucket.setPower(0);
     }
 
@@ -302,7 +313,7 @@ public class QuatrePark extends LinearOpMode {
         servoPinceL.setPower(-1);
         sleep(5);
         servoPinceR.setPower(1);
-        sleep(2000);
+        sleep(1000);
         servoPinceL.setPower(0);
         servoPinceR.setPower(0);
     }
@@ -311,31 +322,53 @@ public class QuatrePark extends LinearOpMode {
         servoPinceL.setPower(1);
         sleep(5);
         servoPinceR.setPower(-1);
-        sleep(2000);
+        sleep(1000);
         servoPinceL.setPower(0);
         servoPinceR.setPower(0);
     }
 
     public boolean notReadyToDump() {
-        return slideMotor.getCurrentPosition() < POSITION_2 - 25 || slideMotor.getCurrentPosition() > POSITION_2 + 25;
+        return slideMotor.getCurrentPosition() < POSITION_2 - 50 || slideMotor.getCurrentPosition() > POSITION_2 + 50;
     }
 
     public boolean notReadyToPick() {
-        return slideMotor.getCurrentPosition() < POSITION_1 - 25 || slideMotor.getCurrentPosition() > POSITION_1 + 25;
+        return slideMotor.getCurrentPosition() < POSITION_1 - 50 || slideMotor.getCurrentPosition() > POSITION_1 + 50;
     }
 
     public void openArm() {
         armMotor.setPower(0.25);
     }
-
+public void openArmPowerfull() {
+        armMotor.setPower(0.8);
+        sleep(500);
+        armMotor.setPower(0);
+    }
     public void closeArm() {
-        armMotor.setPower(-0.5);
-        sleep(1000);
+        armMotor.setPower(-1.0);
+        sleep(700);
         armMotor.setPower(0);
     }
 
     public void stopArm() {
         armMotor.setPower(0);
     }
+    public void stopWheel(){
+        leftFrontDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightBackDrive.setPower(0);
+
+    }
+    public void openClawNoStop(){
+        servoPinceL.setPower(1);
+        sleep(5);
+        servoPinceR.setPower(-1);
+    }
+    public void stopClaw() {
+        servoPinceL.setPower(0);
+        sleep(5);
+        servoPinceR.setPower(0);
+    }
+
 
 }
